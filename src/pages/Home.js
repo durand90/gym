e2ddab6ffd2619955d7237c6402bcc773c5cafe6
Fanfan durand <durand90@live.com>
2022-12-08 13:25:13 -0500

    do it

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React, { useState} from 'react';
+import { Box } from '@mui/material';
+
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
+import Exercises from '../components/Exercises';
+const Home = () => {
+  return (
+    <Box>
+        <HeroBanner />
+        <SearchExercises />
+        <Exercises />
+    </Box>
+  )
+}
+
+export default Home
