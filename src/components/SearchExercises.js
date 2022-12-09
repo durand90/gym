f86400c1e4acfc904bf6d76ff47c8e3700bf9933
Fanfan durand <durand90@live.com>
2022-12-09 16:39:52 -0500

    ok

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () => {
   return (
