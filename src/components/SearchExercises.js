53e37d24075c36957432364c7f60beda4d517ac4
Fanfan durand <durand90@live.com>
2022-12-09 16:51:35 -0500

    b

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -6,8 +6,8 @@ const SearchExercises = () => {
     <Stack alignItems="center" mt="37px" 
     justifyContent="center" p="20px">
       <Typography fontWeight={700} sx={{
-        fontSize: { lg: '44px'}
-      }}>
+        fontSize: { lg: '44px', xs: '30px'}
+      }} mb="50px" textAlign="center">
         Awesome Exercises You <br/>
          Should Know
       </Typography>
