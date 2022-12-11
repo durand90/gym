53d9f747853659c49cbb4aadf78abaac5d60c49a
Fanfan durand <durand90@live.com>
2022-12-10 22:04:16 -0500

    a

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -39,7 +39,7 @@ const HeroBanner = () => {
                 Excercise
             </Typography>
             <img src={HeroBannerImage} alt="banner" 
-            className="hero-banner-img" />
+            className="hero-banner-img" />a
     </Box>
   )
 }
